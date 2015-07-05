9bdaa4b4a13d2d516fa8c420ac9e8d15fa1a22f3
Sira Cuervo <fl.siracusa@outlook.com.ar>
2015-07-05 20:15:36 -0300

    Axxionize 2

--- omni_find7op.mk
+++ omni_find7op.mk
@@ -23,22 +23,22 @@
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
 
 # Get the prebuilt list of APNs
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/axxion/config/gsm.mk)
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # must be before including omni part
-TARGET_BOOTANIMATION_SIZE := 1080x720
+TARGET_BOOTANIMATION_SIZE := 1080
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/axxion/config/common.mk)
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/oppo/find7op/device.mk)
 
 # Discard inherited values and use our own instead.
-PRODUCT_NAME := omni_find7op
+PRODUCT_NAME := axxion_find7op
 PRODUCT_DEVICE := find7op
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := A0001
